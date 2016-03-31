e88c64820ab5292ad2a4bbfa02ce54bdf018b66b
nicknem <antonin.archer@hec.edu>
2016-03-31 15:48:39 +0200

    Fix post call
    Post call to Wikipedia didn't work because of the absence of
    PreventDefault on the click.
    
    * Add prevent default on event.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-$(document).ready(function() {
+$( document ).ready(function() {
   // When click on submit record the query string
-  $("#query-submit").click(function() {
+  $( "#query-submit" ).click(function( event ) {
+    event.preventDefault();
     var query = $("#query-string").val();
     // Append query string to the URL
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + query + "&limit=10&utf8=1";
@@ -12,11 +13,13 @@ $(document).ready(function() {
         type: 'POST',
         headers: { 'Api-User-Agent': 'Example/1.0' },
         success: function(data) {
-          // var titles = data[1];
+          var titles = data[1];
           // var content = data[2];
           // var links = data[3];
-          console.log("YAYYY");
-          console.log(data);
+          console.log( titles );
+        },
+        fail: function(data) {
+          alert( "Oops... Something went wrong!");
         }
     } );
   });
