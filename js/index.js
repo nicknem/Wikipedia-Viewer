3217c03bf84a9d67f18355d31a7add4f41214966
nicknem <antonin.archer@hec.edu>
2016-05-23 19:38:09 +0100

    Refresh list with each query

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ $( document ).ready(function() {
   // When click on submit record the query string
   $( "#query-submit" ).click(function( event ) {
     event.preventDefault();
+    // Empty HTML
+    $( ".results" ).empty();
     var query = $("#query-string").val();
+    $("#query-string").val('');
     // Append query string to the URL
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + query + "&limit=10&utf8=1";
     // GET 10 relevant articles from Wikipedia API
@@ -15,9 +18,10 @@ $( document ).ready(function() {
           var titles = data[1];
           var content = data[2];
           var links = data[3];
-          // for each element of the list
+          // Insert Wikipedia articles
           for (i = 0; i < data.length; i++) {
-            $( ".result-item" ).append("<h2>" + i + ". " + titles[i] + "</h2>" + "<p>" + content[i] + " - see more at: " + links[i] + "</p>");
+            $( ".results" ).append("<h3>" + i + ". " + titles[i] + "</h3>" + "<p>" + content[i] + " - see more at: " + links[i] + "</p>");
+            // And one content
           }
         },
         fail: function(data) {
